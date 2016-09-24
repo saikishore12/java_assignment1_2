7c2cc524cb205d901f6eaef8ed05f05f8436b7a9
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-09-24 20:54:06 +0530

    Assignment1_2

---
+++ src/Assignment2.java
@@ -0,0 +1,18 @@
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		int a = 20;
+
+		int f = a-- - --a;
+		System.out.println("f: " + f);
+		int b=10;
+		int c = a--;
+		System.out.println("c: " + c);
+		int d = a >> 2;
+		System.out.println("d: " + d);
+		int e = a & b;
+		System.out.println("e: " + e);
+
+	}
+}
